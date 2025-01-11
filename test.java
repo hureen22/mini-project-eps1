0669ca67dc1664b50ff6bb9de6e2a0aea4304412
hureen22 <2023871696@student.uitm.edu.my>
2025-01-11 14:09:06 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SimpleGreeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for the user's name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Greet the user
+        System.out.println("Hello, " + name + "! Welcome to Java programming.");
+
+        scanner.close();
+    }
+}
